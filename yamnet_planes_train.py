3026d0f7fbd84d0e7561e0707eb6b871b35ad0e3
jmariscal-harana <jorge.mariscal_harana@kcl.ac.uk>
2020-09-02 22:12:51 +0200

    Changed location of paths; changed path_model to path_yamnet; added yamnet to the name of the feature/label .npy files

--- yamnet_planes_train.py
+++ yamnet_planes_train.py
@@ -1,6 +1,7 @@
 import pyaudio, librosa
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import tensorflow as tf
 
 # Decide what type of messages are displayed by TensorFlow (ERROR, WARN, INFO, DEBUG, FATAL)
@@ -12,10 +13,10 @@ tf.compat.v1.disable_eager_execution()
 # OPTION 2: maximum memory allocation per session (0-1 = 0-100%)
 tf_ver = tf.__version__
 if tf_ver[0] == "1":
-    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.05)
+    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=1.00)
     sess=tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 elif tf_ver[0] == "2":
-    gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.05)
+    gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=1.00)
     sess=tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 
 
@@ -23,12 +24,14 @@ elif tf_ver[0] == "2":
 import os, sys
 
 path_root = '/home/ups/Proyectos/Vigia_sonido/' #path to root folder
-path_model = path_root+"Models/yamnet_planes/"
-# path_model = input("Enter the path of your repository: ") # ask user for path_model
-assert os.path.exists(path_model)
-sys.path.append(path_model)
+path_yamnet = path_root+"Models/yamnet_planes/"
+# path_yamnet = input("Enter the path of your repository: ") # ask user for path_yamnet
+assert os.path.exists(path_yamnet)
+sys.path.append(path_yamnet)
+path_data_train = path_root+"Datasets/airplanes_v3/training_data/"
+# path_data_train = input("Enter the path of your training dataset: ") # Ask user for path_data_train
 
-path_yamnet_original = path_model+'yamnet_original/' #path to original yamnet files
+path_yamnet_original = path_yamnet+'yamnet_original/' #path to original yamnet files
 assert os.path.exists(path_yamnet_original)
 
 
@@ -42,13 +45,10 @@ import yamnet_modified as yamnet_modified
 params.PATCH_HOP_SECONDS = 0.096 #low values: higher accuracy but higher computational cost
 
 yamnet_features = yamnet_modified.yamnet_frames_model(params)
-yamnet_features.load_weights(path_model+'yamnet.h5')
+yamnet_features.load_weights(path_yamnet+'yamnet.h5')
 
 
 # Count class samples, perform data augmentation, or load a specific augmented dataset
-path_data_train = path_root+"Datasets/airplanes_v3/training_data/"
-# path_data_train = input("Enter the path of your training dataset: ") # Ask user for path_data_train
-
 # Count the number of original samples for each class
 min_sample_seconds=params.PATCH_WINDOW_SECONDS  # Should be at least equal to params.PATCH_WINDOW_SECONDS
 max_sample_seconds=1000.0
@@ -64,12 +64,12 @@ sample_numbers = [0]
 #     DESIRED_SR=params.SAMPLE_RATE)
 
 # Based on the number of original samples decide how much data augmentation is required by each class
-num_augmentations=[0,7]
-perform_augmentation = True
+num_augmentations=[0,0]
+perform_augmentation = False
 
 patch_hop_seconds_str = str(params.PATCH_HOP_SECONDS).replace('.','')
-features_str = 'features_'+patch_hop_seconds_str+'_'+str(num_augmentations[0])+'_'+str(num_augmentations[1]) 
-labels_str = 'labels_'+patch_hop_seconds_str+'_'+str(num_augmentations[0])+'_'+str(num_augmentations[1]) 
+features_str = 'yamnet_features_'+patch_hop_seconds_str+'_'+str(num_augmentations[0])+'_'+str(num_augmentations[1]) 
+labels_str = 'yamnet_labels_'+patch_hop_seconds_str+'_'+str(num_augmentations[0])+'_'+str(num_augmentations[1]) 
 
 if perform_augmentation == True:
     samples, labels = yamnet_functions.data_augmentation(
@@ -169,8 +169,8 @@ import datetime
 # Callbacks
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
 time_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-path_model_save = path_model+'saved_models/top_model_'+time_now+'.hdf5'
-save_best = ModelCheckpoint(path_model_save, save_best_only=True, monitor='val_loss', mode='min')
+path_yamnet_save = path_yamnet+'saved_models/top_model_'+time_now+'.hdf5'
+save_best = ModelCheckpoint(path_yamnet_save, save_best_only=True, monitor='val_loss', mode='min')
 # log_dir = "logs/{}".format(int(time()))
 # tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
@@ -188,11 +188,9 @@ val_loss = history.history['val_loss']
 val_acc = history.history['val_accuracy']
 
 # Write metadata file containing hyperparameters for data augmentation and training
-import pandas as pd
-
 metadata_headers = ['path_data_train','patch_hop_seconds','samples','aug','samples_aug','classifier_conf','optimiser','optimiser_params','loss_function','path_model','epochs','val_split','train_loss','train_accuracy','val_loss','val_accuracy']
 # metadata_values = np.zeros((1,len(metadata_headers)), dtype=int)
-metadata_values = [path_data_train, params.PATCH_HOP_SECONDS, sample_numbers, num_augmentations, counts.tolist(), num_hidden, opt_type, opt_conf, loss_type, path_model_save, epochs, val_split, train_loss, train_acc, val_loss, val_acc]
+metadata_values = [path_data_train, params.PATCH_HOP_SECONDS, sample_numbers, num_augmentations, counts.tolist(), num_hidden, opt_type, opt_conf, loss_type, path_yamnet_save, epochs, val_split, train_loss, train_acc, val_loss, val_acc]
 metadata_df = pd.DataFrame([metadata_values],columns=metadata_headers)
 path_metadata = path_data_train+'metadata_'+patch_hop_seconds_str+'_'+str(num_augmentations[0])+'_'+str(num_augmentations[1])+'_'+time_now+'.csv'
 metadata_df.to_csv(path_metadata,index=False)
