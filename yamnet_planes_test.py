91fd9984bf1ffe949e23c215d7ac0f1bfe4d03d3
jmariscal-harana <jorge.mariscal_harana@kcl.ac.uk>
2020-09-22 19:45:27 +0200

    Loop for multiple datasets/inference hops; thresholding for ROC and PR plots

--- yamnet_planes_test.py
+++ yamnet_planes_test.py
@@ -34,75 +34,154 @@ import yamnet_functions
 import yamnet_original.params as params
 import yamnet_modified as yamnet_modified
 
-params.PATCH_HOP_SECONDS = 0.096 # During testing, this should match the imposed inference time (0.48s ~= 2Hz)
+params.PATCH_HOP_SECONDS = 0.24 # During testing, this should match the imposed inference time (0.48s ~= 2Hz)
 DESIRED_SR = params.SAMPLE_RATE # required by YAMNet
+
+# Model name TODO: extract from metadata
+# datetime = '20200824_234802' 	# Undersampling
+# datetime = '20200823_025302'  # Data aug
+# datetime = '20200823_210326'	# Data aug*2
+# datetime = '20200825_011907'	# Hybrid
+
+PATCH_HOP_SECONDS = [0.96, 0.48, 0.24, 0.096]
+datetimes = ['20200824_234802', '20200823_025302', '20200823_210326', '20200825_011907']
+
+# for params.PATCH_HOP_SECONDS in PATCH_HOP_SECONDS:
 
 yamnet_features = yamnet_modified.yamnet_frames_model(params)
 yamnet_features.load_weights(path_model+'yamnet.h5')
 
-
-# Load model
+# Load model and test
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import SGD, Adam
 
-datetime = '20200825_011907' # TODO: extract from metadata
-path_model_save = path_model+'saved_models/top_model_'+datetime+'.hdf5'
+for datetime in datetimes:
 
-yamnet_planes = load_model(path_model_save)    
-opt = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)    # TODO: extract from metadata
-yamnet_planes.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # TODO: extract from metadata???
-yamnet_planes.summary()
+    path_model_save = path_model+'saved_models/yamnet_'+datetime+'.hdf5'
 
-import yamnet_original.params as params
-DESIRED_SR = params.SAMPLE_RATE # required by YAMNet
+    yamnet_planes = load_model(path_model_save)    
+    opt = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)    # TODO: extract from metadata
+    yamnet_planes.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # TODO: extract from metadata???
+    yamnet_planes.summary()
 
-# Scores for testing folder
-prediction_confidence = 0.5 # [0.5:1.0) where 0.5 is full confidence and ~1.0 is little confidence
-class_labels = ["not_plane", "plane"]   # TODO: extract from metadata
-path_data_test = path_root+'Datasets/airplanes_v3/holdout_data/'
+    import yamnet_original.params as params
+    DESIRED_SR = params.SAMPLE_RATE # required by YAMNet
 
-detection_rate = []
+    class_labels = ["not_plane", "plane"]   # TODO: extract from metadata
+    path_data_test = path_root+'Datasets/airplanes_v3/holdout_data/'
 
-for reference_class in range(len(class_labels)):
-    path_data_test_class = path_data_test+class_labels[reference_class]+'/'
-    arr = os.listdir(path_data_test_class)
+    # Scores for testing folder
+    test_mode = 2 # 1: proportional thresholding, 2: ROC thresholding
 
-    # path_data_test_class = path_model
-    # arr = ['tractor.wav']
+    processed_samples = [0, 0]
 
-    predicted_class = []
+    # Method 1: apply equal threshold to all classes
+    if test_mode == 1:
+        prediction_confidence = 0.5 # [0.5:1.0) where 0.5 is full confidence and ~1.0 is little confidence
+        
+        not_discarded = [0, 0]
+        detection_rate = []
 
-    for fname in arr:
-        print(fname)
-        fname = path_data_test_class+fname
-        waveform = yamnet_functions.read_wav(fname, DESIRED_SR, use_rosa=1)
-        # make file a bit longer by duplicating it 
-        # waveform = np.concatenate((waveform,waveform,waveform))
-        scores = yamnet_functions.run_models(waveform, yamnet_features, yamnet_planes, strip_silence=False)
-        scores = np.array(scores)
+        for reference_class, class_label in enumerate(class_labels):
+            path_data_test_class = path_data_test+class_label+'/'
+            arr = os.listdir(path_data_test_class)
 
-        prediction_loc, _ = np.where(scores>=prediction_confidence)
-        scores = scores[prediction_loc, :]
-        if prediction_loc.size == 0:
-            print("Prediction not available for current prediction confidence: {}".format(prediction_confidence))
-            continue
+            predicted_class = []
 
-        if scores[0][0] != -1:
-            predicted_class = np.append(predicted_class, scores.argmax(1))
-            scores_mean = np.mean(scores, axis=0)
-            winner_mean = class_labels[scores_mean.argmax()]
-            print(" Best score: {}  label: {}".format(scores_mean.max(), winner_mean))
+            for fname in arr:
 
-    detection_rate = np.append(detection_rate, sum(predicted_class==reference_class)/len(predicted_class)*100)
-    print('True positive rate for {} class: {:4.2f}%'.format(class_labels[reference_class], detection_rate[reference_class]))
-    print('False positive rate for {} class: {:4.2f}%'.format(class_labels[reference_class], 100-detection_rate[reference_class]))
+                print(fname)
+                fname = path_data_test_class+fname
+                waveform = yamnet_functions.read_wav(fname, DESIRED_SR, use_rosa=1)
 
-# Write results file containing performance metrics
-import pandas as pd
+                scores = yamnet_functions.run_models(waveform, yamnet_features, yamnet_planes, strip_silence=False)
+                scores = np.array(scores)
+                if scores[0][0] == -1:
+                    continue
+                        
+                processed_samples[reference_class] += len(scores)
+                prediction_loc, _ = np.where(scores>=prediction_confidence)
+                scores = scores[prediction_loc, :]
+                not_discarded[reference_class] += len(scores)
 
-patch_hop_seconds_str = str(params.PATCH_HOP_SECONDS).replace('.','')
-results_headers = ['path_model_save','patch_hop_seconds','reference_classes','prediction_confidence','TPR','FNR']
-results_values = [path_model_save, patch_hop_seconds_str, class_labels, prediction_confidence, detection_rate, 100-detection_rate]
-results_df = pd.DataFrame([results_values],columns=results_headers)
-path_results = path_data_test+'test_'+os.path.basename(path_model_save)[:-5]+'_'+patch_hop_seconds_str+'_'+str(round(100*prediction_confidence))+'.csv'
-results_df.to_csv(path_results,index=False)
+                if prediction_loc.size == 0:
+                    print("Prediction not available for current prediction confidence: {}".format(prediction_confidence))
+                    continue
+            
+                predicted_class = np.append(predicted_class, scores.argmax(1)).astype(int)
+                scores_mean = np.mean(scores, axis=0)
+                winner_mean = class_labels[scores_mean.argmax()]
+                print(" Best score: {}  label: {}".format(scores_mean.max(), winner_mean))
+                
+            detection_rate = np.append(detection_rate, sum(predicted_class == reference_class) / len(predicted_class)*100)
+            print('True positive rate for {} class: {:4.2f}%'.format(class_label, detection_rate[reference_class]))
+            print('False positive rate for {} class: {:4.2f}%'.format(class_label, 100-detection_rate[reference_class]))
+
+
+    # Method 2: apply moving threshold for ROC plots
+    elif test_mode == 2:
+        prediction_threshold_step = 0.01
+
+        prediction_thresholds = np.arange(0, 1+prediction_threshold_step, prediction_threshold_step) # [0.0:1.0] where 0.5 is equal weighting for both classes
+        prediction_thresholds = prediction_thresholds.reshape(len(prediction_thresholds), 1)
+        detection_rate = np.empty((len(prediction_thresholds), 0), int)
+
+        for reference_class, class_label in enumerate(class_labels):
+            path_data_test_class = path_data_test+class_label+'/'
+            arr = os.listdir(path_data_test_class)
+
+            predicted_class = np.empty((len(prediction_thresholds), 0), int)
+
+            for fname in arr:
+
+                print(fname)
+                fname = path_data_test_class+fname
+                waveform = yamnet_functions.read_wav(fname, DESIRED_SR, use_rosa=1)
+
+                scores = yamnet_functions.run_models(waveform, yamnet_features, yamnet_planes, strip_silence=False)
+                scores = np.array(scores)
+                if scores[0][0] == -1:
+                    continue
+
+                processed_samples[reference_class] += len(scores)
+                scores = scores[:, 0]
+                # if reference_class == 0: # TODO: make this automatic
+                #     print('WARNING - HARDCODED! Randomly downsampling the "not_plane" class to achieve class balance')
+                #     random
+
+                predicted_class = np.append(predicted_class, (scores <= prediction_thresholds), axis=1).astype(int)
+                
+            detection_rate_current = np.sum((predicted_class == reference_class), axis=1) / predicted_class.shape[1] * 100
+            detection_rate = np.append(detection_rate, detection_rate_current.reshape(len(prediction_thresholds), 1), axis=1)
+
+            for loc, prediction_threshold in enumerate(prediction_thresholds):
+                print('True positive rate for {} class (threshold = {:.2f}): {:4.2f}%'.format(class_label, prediction_threshold[0], detection_rate[loc, reference_class]))
+                print('False positive rate for {} class (threshold = {:.2f}): {:4.2f}%'.format(class_label, prediction_threshold[0], 100-detection_rate[loc, reference_class]))
+
+
+    # Write results file containing performance metrics
+    import pandas as pd
+
+    patch_hop_seconds_str = str(params.PATCH_HOP_SECONDS).replace('.','')
+
+    if test_mode == 1:
+        results_headers = ['path_model_save','patch_hop_seconds','reference_classes','prediction_confidence','TP','FN','total_proc','not_discarded_by_threshold']
+        results_values = [path_model_save, patch_hop_seconds_str, class_labels, prediction_confidence, detection_rate, 100-detection_rate, processed_samples, not_discarded]
+        path_results = path_data_test+'test_'+os.path.basename(path_model_save)[:-5]+'_'+patch_hop_seconds_str+'_'+str(round(100*prediction_confidence))+'_'+str(not_discarded)+'.csv'
+    elif test_mode == 2:
+        results_headers = ['path_model_save','patch_hop_seconds','reference_classes','threshold','TP','FN','TN','FP','total_proc']
+        TP = [int(x) for x in detection_rate[:,1]/100*processed_samples[1]]
+        FN = [int(x) for x in (100-detection_rate[:,1])/100*processed_samples[1]]
+        TN = [int(x) for x in detection_rate[:,0]/100*processed_samples[0]]
+        FP = [int(x) for x in (100-detection_rate[:,0])/100*processed_samples[0]]
+        results_values = [path_model_save, patch_hop_seconds_str, class_labels, prediction_thresholds.T[0], TP, FN, TN, FP, processed_samples]
+        path_results = path_data_test+'test_'+os.path.basename(path_model_save)[:-5]+'_'+patch_hop_seconds_str+'_thresholding.csv'
+        
+        results_headers_ROC_PR = ['TP','FN','TN','FP']
+        results_values_ROC_PR = np.array([TP, FN, TN, FP]).T
+        path_results_ROC_PR = path_data_test+'test_'+os.path.basename(path_model_save)[:-5]+'_'+patch_hop_seconds_str+'_thresholding_ROC_PR.csv'
+        results_df = pd.DataFrame(results_values_ROC_PR,columns=results_headers_ROC_PR)
+        results_df.to_csv(path_results_ROC_PR,index=False)
+
+    results_df = pd.DataFrame([results_values],columns=results_headers)
+    results_df.to_csv(path_results,index=False)
