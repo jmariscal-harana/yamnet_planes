448dffb182c2e739f2d1dad430eb20506734abf2
jmariscal-harana <jorge.mariscal_harana@kcl.ac.uk>
2021-01-12 12:23:57 +0100

    Clean up; two ways of extracting/saving/loading features: per class and per sample; plot and save training progress

--- yamnet_planes_train.py
+++ yamnet_planes_train.py
@@ -4,23 +4,24 @@ import pyaudio, librosa, os, sys, datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import tensorflow as tf
+# import tensorflow as tf
 from time import time
+from tqdm import tqdm
 
 # Decide what type of messages are displayed by TensorFlow (ERROR, WARN, INFO, DEBUG, FATAL)
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # Disable eager execution for TF1 compatibility
-tf.compat.v1.disable_eager_execution()
+# tf.compat.v1.disable_eager_execution()
 
 # OPTION 2: maximum memory allocation per session (0-1 = 0-100%)
-tf_ver = tf.__version__
-if tf_ver[0] == "1":
-    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.45)
-    sess=tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-elif tf_ver[0] == "2":
-    gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.45)
-    sess=tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
+# tf_ver = tf.__version__
+# if tf_ver[0] == "1":
+#     gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+#     sess=tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+# elif tf_ver[0] == "2":
+#     gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.95)
+#     sess=tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 
 
 ##########################################
@@ -28,7 +29,8 @@ elif tf_ver[0] == "2":
 path_root = '/home/ups/Proyectos/vigia-sonido/' #path to root folder
 path_yamnet = os.path.join(path_root, 'Models/yamnet_planes/')
 path_yamnet_original = os.path.join(path_yamnet, 'yamnet_original/') #path to original yamnet files
-path_data_train = os.path.join(path_root, 'Datasets/small_aircraft_v1/training_data/')
+path_data_train = os.path.join(path_root, 'Datasets/small_aircraft_v2/training_data/')
+path_data_csv = os.path.join(path_root, 'Datasets/small_aircraft_v2/data_csv/')
 path_yamnet_save = os.path.join(path_root, 'Models_saved/yamnet/')
 
 assert os.path.exists(path_yamnet)
@@ -57,7 +59,7 @@ yamnet_features.load_weights(path_yamnet+'yamnet.h5')
 
 ##########################################
 # DATA ANALYSIS
-# Count the number of original samples for each class
+# Count the number of original features for each class
 min_sample_seconds=params.PATCH_WINDOW_SECONDS  # Should be at least equal to params.PATCH_WINDOW_SECONDS
 max_sample_seconds=1000.0
 
@@ -74,9 +76,9 @@ sample_numbers = [0]
 
 ##########################################
 # DATA AUGMENTATION and FEATURE EXTRACTION
-# Based on the number of original samples decide how much data augmentation is required by each class
-scenario = ['und', 'aug1', 'aug2', 'hyb']
-scenario = scenario[2]
+# Based on the number of original features decide how much data augmentation is required by each class
+scenarios = ['und', 'aug1', 'aug2', 'hyb']
+scenario = scenarios[0]
 
 if scenario == 'und':
     num_augmentations=[0,0]
@@ -89,61 +91,150 @@ elif scenario == 'hyb':
 else:
     raise NameError(scenario+' is not a valid scenario')
 
-perform_augmentation = False
 
-path_features = os.path.join(path_data_train, 'features', 'yamnet','yamnet_features_'+patch_hop_seconds_str+'_'+scenario)
-path_labels = os.path.join(path_data_train, 'features', 'yamnet','yamnet_labels_'+patch_hop_seconds_str+'_'+scenario)
+feature_extraction_method = 1
+path_data_csv_file = os.path.join(path_data_csv, 'audio_paths_20201214_205907.csv')
 
-if perform_augmentation == True:
-    samples, labels = yamnet_functions.data_augmentation(
-        path_data_train, 
-        yamnet_features,
-        num_augmentations=num_augmentations,
-        min_sample_seconds=min_sample_seconds,
-        max_sample_seconds=max_sample_seconds,
-        use_rosa=True,
-        DESIRED_SR=params.SAMPLE_RATE)
 
-    # Randomise sample/label order
-    import random
+# OPTION 1: OLD SCHOOL
+perform_augmentation = True
+classes = ['not_plane', 'plane']
 
-    idxs = list(range(len(labels)))
-    random.shuffle(idxs)
-    samples = [samples[i] for i in idxs]
-    labels = [labels[i] for i in idxs]
+if feature_extraction_method == 0:
+    path_features = os.path.join(path_data_train, 'features', 'yamnet','yamnet_features_'+patch_hop_seconds_str+'_'+scenario)
+    path_labels = os.path.join(path_data_train, 'features', 'yamnet','yamnet_labels_'+patch_hop_seconds_str+'_'+scenario)
 
-    # NumPy arrays are more efficient, especially for large arrays
-    samples = np.array(samples)
-    labels = np.array(labels)
+    if perform_augmentation == True:
+        features, labels = yamnet_functions.data_augmentation(
+            path_data_train, 
+            classes,
+            yamnet_features,
+            num_augmentations=num_augmentations,
+            min_sample_seconds=min_sample_seconds,
+            max_sample_seconds=max_sample_seconds,
+            use_rosa=True,
+            DESIRED_SR=params.SAMPLE_RATE)
 
-    # To ensure a balanced dataset, randomly delete [samples,labels] from other classes to match number of samples of least frequent class
-    _, counts = np.unique(labels, return_counts=True)
-    idx_locs_delete = []
+        # Randomise sample/label order
+        # TODO: use this function
+        # features, labels = balance_classes(features, labels)
 
-    for idx in np.unique(labels):
-        idx_loc = np.where(labels==idx)[0]
+        import random
 
-        if len(idx_loc) > counts.min():
-            idx_locs_delete = np.append(idx_locs_delete,idx_loc[counts.min()-1:-1])
+        idxs = list(range(len(labels)))
+        random.shuffle(idxs)
+        features = [features[i] for i in idxs]
+        labels = [labels[i] for i in idxs]
 
-    labels = np.delete(labels, idx_locs_delete.astype(int))
-    samples = np.delete(samples, idx_locs_delete.astype(int), axis=0)
+        # NumPy arrays are more efficient, especially for large arrays
+        features = np.array(features)
+        labels = np.array(labels)
 
-    # Save features and corresponding labels
-    np.save(path_features, samples)
-    np.save(path_labels, labels)
+        # To ensure a balanced dataset, randomly delete [features,labels] from other classes to match number of features of least frequent class
+        _, counts = np.unique(labels, return_counts=True)
+        idx_locs_delete = []
+
+        for idx in np.unique(labels):
+            idx_loc = np.where(labels==idx)[0]
+
+            if len(idx_loc) > counts.min():
+                idx_locs_delete = np.append(idx_locs_delete,idx_loc[counts.min()-1:-1])
+
+        labels = np.delete(labels, idx_locs_delete.astype(int))
+        features = np.delete(features, idx_locs_delete.astype(int), axis=0)
+
+        # Save features and corresponding labels
+        np.save(path_features, features)
+        np.save(path_labels, labels)
+
+# OPTION 2: SAVE INDIVIDUAL FEATURES
+if feature_extraction_method == 1:
+    df_data_csv = pd.read_csv(path_data_csv_file)
+    classes = np.unique(df_data_csv.iloc[:,1]).tolist()
+
+    for class_idx, class_label in enumerate(classes):
+        df_data_csv_class = df_data_csv[df_data_csv.iloc[:,1]==class_label]
+        path_audios =  df_data_csv_class.iloc[:,0].tolist()
+        
+        # Save as audio name -> hop size -> augment cycle
+        for path_audio in tqdm(path_audios):
+            yamnet_functions.save_features(
+                path_audio,
+                params.SAMPLE_RATE,
+                path_data_train,
+                patch_hop_seconds_str,
+                num_augmentations,
+                class_idx,
+                yamnet_features)
 
 
 ##########################################
 # FEATURE DATA LOADING
-samples = np.load(path_features + '.npy')
-labels = np.load(path_labels + '.npy')
+if feature_extraction_method == 0:
+    features = np.load(path_features + '.npy')
+    labels = np.load(path_labels + '.npy')
 
-_, counts = np.unique(labels, return_counts=True)
+    _, counts = np.unique(labels, return_counts=True)
 
-print("\nSample size: {} and type: {}".format(samples.shape, samples.dtype))
-print("Label size:  {}".format(labels.shape))
-print("Sample distribution per class (after balancing): {}\n".format(counts))
+    print("\nSample size: {} and type: {}".format(features.shape, features.dtype))
+    print("Label size:  {}".format(labels.shape))
+    print("Sample distribution per class (after balancing): {}\n".format(counts))
+
+
+# OPTION 2: SAVE INDIVIDUAL FEATURES
+# Load all features and labels and save them into the same vector
+elif feature_extraction_method == 1:
+    print('Loading features and labels.\n')
+    features = []
+    labels = []
+
+    # 1. read csv
+    df_data_csv = pd.read_csv(path_data_csv_file)
+    classes = np.unique(df_data_csv.iloc[:,1]).tolist()
+
+    for class_idx, class_label in enumerate(classes):
+        print('\nLoading class {}\n'.format(class_label))
+        
+        df_data_csv_class = df_data_csv[df_data_csv.iloc[:,1]==class_label]
+        path_audios =  df_data_csv_class.iloc[:,0].tolist()
+
+        # Load features and labels for each audio and append
+        for idx, path_audio in enumerate(tqdm(path_audios)):
+            audio_filename = ('_').join(path_audio.split(os.path.sep)[-2:]).split('.')[0]
+            path_features = os.path.join(path_data_train, 'features', 'yamnet', audio_filename + '_features_' + patch_hop_seconds_str)
+            path_labels = os.path.join(path_data_train, 'features', 'yamnet', audio_filename + '_labels_' +  patch_hop_seconds_str) 
+
+            for idx_aug in range(num_augmentations[class_idx] + 1):
+                path_features_tmp = path_features + '_{:02d}'.format(idx_aug) + '.npy'
+                path_labels_tmp = path_labels + '_{:02d}'.format(idx_aug) + '.npy'
+                
+                if os.path.isfile(path_features_tmp) and os.path.isfile(path_labels_tmp):
+                    features_tmp = np.load(path_features_tmp)
+                    labels_tmp = np.load(path_labels_tmp)
+
+                    for feature, label in zip(features_tmp, labels_tmp):
+                        features.append(feature)
+                        labels.append(label)
+
+                else:
+                    print('WARNING: Cannot load file {}; it does not exist.'.format(audio_filename))
+
+
+    ##########################################
+    # FEATURE BALANCING
+    print('Balancing class features.\n')
+    _, counts = np.unique(labels, return_counts=True)
+    print("Sample distribution per class (before balancing): {}\n".format(counts))
+
+    features, labels = yamnet_functions.balance_classes(features, labels)
+
+    _, counts = np.unique(labels, return_counts=True)
+    print("Sample size: {} and type: {}".format(len(features), type(features[0])))
+    print("Label size:  {}".format(len(labels)))
+    print("Sample distribution per class (after balancing): {}\n".format(counts))
+
+    features = [features] # Required when passing it to model.fit
+    labels = [labels] # Required when passing it to model.fit
 
 
 ##########################################
@@ -175,6 +266,7 @@ yamnet_planes = yamnet_classifier(
 # Optimisation configuration
 opt_type = 'SGD'
 loss_type = 'sparse_categorical_crossentropy'
+metrics = ['accuracy']
 
 if opt_type == 'Adam':
     opt_conf = [0.001]
@@ -185,27 +277,33 @@ elif opt_type == 'SGD':
 else:
     raise NameError(opt_type+' is not a valid optimiser')
 
-yamnet_planes.compile(optimizer=opt, loss=loss_type, metrics=['accuracy'])
+yamnet_planes.compile(optimizer=opt, loss=loss_type, metrics=metrics)
 
 
 ##########################################
 # TRAINING
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
+print('##########################################')
+print('Training time, baby!\nScenario: {}'.format(scenario))
+print('##########################################')
+
+time_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+path_yamnet_save_model = os.path.join(path_yamnet_save, time_now+'_yamnet_'+patch_hop_seconds_str+'_'+scenario+'.hdf5')
+path_yamnet_save_plot = os.path.join(path_yamnet_save, time_now+'_progress_'+patch_hop_seconds_str+'_'+scenario+'.png')
+
 # Callbacks
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
-time_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-path_yamnet_save_file = os.path.join(path_yamnet_save, time_now+'_yamnet_'+patch_hop_seconds_str+'_'+scenario+'.hdf5')
-
-save_best = ModelCheckpoint(path_yamnet_save_file, save_best_only=True, monitor='val_loss', mode='min')
+save_best = ModelCheckpoint(path_yamnet_save_model, save_best_only=True, monitor='val_loss', mode='min')
+plot_metrics = yamnet_functions.TrainingPlot(path_yamnet_save_plot) # real-time plots during training
 # log_dir = "logs/{}".format(int(time()))
 # tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-epochs = 3000
+epochs = 1000
 val_split = 0.1
 
 time_start = time()
-history = yamnet_planes.fit(samples, labels, epochs=epochs, validation_split=val_split, callbacks=[save_best])
+history = yamnet_planes.fit(features, labels, epochs=epochs, validation_split=val_split, callbacks=[save_best,plot_metrics])
 time_end = time()
 time_train = round(time_end - time_start)
 
@@ -218,7 +316,7 @@ val_acc = history.history['val_accuracy']
 ##########################################
 # METADATA
 # Write metadata file containing hyperparameters for data augmentation and training
-metadata_headers = ['path_data_train','patch_hop_seconds','samples','aug','samples_aug','classifier_conf','optimiser','optimiser_params','loss_function','path_model','epochs','val_split','train_loss','train_accuracy','val_loss','val_accuracy']
+metadata_headers = ['path_data_train','patch_hop_seconds','features','aug','features_aug','classifier_conf','optimiser','optimiser_params','loss_function','path_model','epochs','val_split','train_loss','train_accuracy','val_loss','val_accuracy']
 # metadata_values = np.zeros((1,len(metadata_headers)), dtype=int)
 metadata_values = [path_data_train, params.PATCH_HOP_SECONDS, sample_numbers, num_augmentations, counts.tolist(), num_hidden, opt_type, opt_conf, loss_type, path_yamnet_save, epochs, val_split, train_loss, train_acc, val_loss, val_acc]
 metadata_df = pd.DataFrame([metadata_values],columns=metadata_headers)
